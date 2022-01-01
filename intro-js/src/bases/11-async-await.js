48cac42a880a3fb98833bbee538e52b015cc5c03
Fernando Ángel -Medellín Cuevas <fer0osennin@gmail.com>
2022-01-01 05:48:00 -0600

    finalizado intro

---
+++ intro-js/src/bases/11-async-await.js
@@ -0,0 +1,38 @@
+const getImagen = async()=>{
+    const apiKey ='z4pZO17j4sAqjAfJc8MS8KSV8whd2fOe'
+    const baseUrl = await fetch( `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
+    const {data} = await baseUrl.json()
+
+    const {url} = data.images.original
+    const img = document.createElement('img')
+    img.src=url
+    document.body.append(img)
+
+    console.log(url)
+}
+
+getImagen()
+
+// FETCH API
+
+// const apiKey ='z4pZO17j4sAqjAfJc8MS8KSV8whd2fOe'
+
+// const baseUrl = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+
+// const peticion = fetch(baseUrl)
+
+
+// // promeasas en cadena
+// peticion
+// .then(resp => resp.json())
+// .then(({data}) =>{
+//     //console.log(data.images.original.url)
+//     // const de desustructuracion
+//     const {url} = data.images.original
+//     //console.log(url)
+//     const img = document.createElement ('img')
+//     img.src = url
+
+//     document.body.append (img)
+// })
+// .catch (console.warn)
---
+++ intro-js/src/bases/12-ternario.js
@@ -0,0 +1,15 @@
+// const activo = true
+// let mensaje=''
+// if(activo){
+//     mensaje='activo'
+// }
+// else{
+//     mensaje='inavtivo'
+// }
+
+// console.log(mensaje)
+const activo = true
+
+const mensaje = activo && 'Activo'
+
+console.log(mensaje)
